c31d78379e4c8246024ffb78b87c4ccb1748d530
m47bell <m.vidals047@gmail.com>
2015-08-01 16:56:15 -0400

    adding alert dialog

---
+++ app/src/main/java/abassawo/c4q/nyc/fe_nyc/AlertDialogFragment.java
@@ -0,0 +1,26 @@
+package abassawo.c4q.nyc.fe_nyc;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by c4q-marbella on 8/1/15.
+ */
+public class AlertDialogFragment extends DialogFragment{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null); //close dialog after click
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+}
