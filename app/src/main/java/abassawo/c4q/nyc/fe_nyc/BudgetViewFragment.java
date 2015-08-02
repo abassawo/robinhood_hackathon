a0419c1504414d1938f3e7049981bc96a130dbd6
tashsmit <tashsmit91@yahoo.com>
2015-08-02 14:51:28 -0400

    piechart/progress bar

--- app/src/main/java/abassawo/c4q/nyc/fe_nyc/BudgetViewFragment.java
+++ app/src/main/java/abassawo/c4q/nyc/fe_nyc/BudgetViewFragment.java
@@ -4,6 +4,7 @@ package abassawo.c4q.nyc.fe_nyc;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -17,6 +18,7 @@ import android.view.animation.AccelerateDecelerateInterpolator;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -29,12 +31,18 @@ import org.achartengine.renderer.SimpleSeriesRenderer;
 
 public class BudgetViewFragment extends Fragment {
     public WebView webView;
+    public ImageView goalImage;
 
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View myInflatedView = inflater.inflate(R.layout.fragment_budget_view, container, false);
+
+        goalImage = (ImageView)myInflatedView.findViewById(R.id.goal);
+        goalImage.setBackgroundResource(R.drawable.goal_animation);
+        AnimationDrawable frameAnimation = (AnimationDrawable) goalImage.getBackground();
+        frameAnimation.start();
 
         webView = (WebView) myInflatedView.findViewById(R.id.web);
         webView.addJavascriptInterface(new WebAppInterface(), "Android");
@@ -43,49 +51,26 @@ public class BudgetViewFragment extends Fragment {
         webView.loadUrl("file:///android_asset/chart.html");
 
         return myInflatedView;
- * Activities that contain this fragment must implement the
- * {@link BudgetViewFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BudgetViewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-
-import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
-
-
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+    }
 
 
     public class WebAppInterface {
 
         @JavascriptInterface
         public int getNum1() {
-            return 1800;
-        }
-
-        @JavascriptInterface
-        public int getNum2() {
             return 320;
         }
 
         @JavascriptInterface
-        public int getNum3() {
+        public int getNum2() {
             return 1285;
         }
 
         @JavascriptInterface
-        public int getNum4() {
-            return 20;
-        }
-
-        @JavascriptInterface
-        public int getNum5() {
+        public int getNum3() {
             return 115;
         }
+
     }
+}
 
--- app/src/main/java/abassawo/c4q/nyc/fe_nyc/MainActivity.java
+++ app/src/main/java/abassawo/c4q/nyc/fe_nyc/MainActivity.java
@@ -1,7 +1,6 @@
 package abassawo.c4q.nyc.fe_nyc;
 
 import android.support.v7.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -9,38 +8,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
-
-
-
-
 import android.app.Activity;
-import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
