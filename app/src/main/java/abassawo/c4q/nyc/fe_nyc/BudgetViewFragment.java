c5eb02c1743e41e3ed41b28cfff963158c9c9c3a
tashsmit <tashsmit91@yahoo.com>
2015-08-02 11:46:36 -0400

    budget display in the works

--- app/src/main/java/abassawo/c4q/nyc/fe_nyc/BudgetViewFragment.java
+++ app/src/main/java/abassawo/c4q/nyc/fe_nyc/BudgetViewFragment.java
@@ -1,75 +1,74 @@
 package abassawo.c4q.nyc.fe_nyc;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.model.SeriesSelection;
+import org.achartengine.renderer.DefaultRenderer;
+import org.achartengine.renderer.SimpleSeriesRenderer;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BudgetViewFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BudgetViewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BudgetViewFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public WebView webView;
 
 
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BudgetViewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BudgetViewFragment newInstance(String param1, String param2) {
-        BudgetViewFragment fragment = new BudgetViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    public BudgetViewFragment() {
-        // Required empty public constructor
-    }
-
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View myInflatedView = inflater.inflate(R.layout.fragment_budget_view, container, false);
+
+        webView = (WebView) myInflatedView.findViewById(R.id.web);
+        webView.addJavascriptInterface(new WebAppInterface(), "Android");
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/chart.html");
+
+        return myInflatedView;
+    }
+
+    public class WebAppInterface {
+
+        @JavascriptInterface
+        public int getNum1() {
+            return 1800;
+        }
+
+        @JavascriptInterface
+        public int getNum2() {
+            return 320;
+        }
+
+        @JavascriptInterface
+        public int getNum3() {
+            return 1285;
+        }
+
+        @JavascriptInterface
+        public int getNum4() {
+            return 20;
+        }
+
+        @JavascriptInterface
+        public int getNum5() {
+            return 115;
         }
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_budget_view, container, false);
-    }
-
-
-
-
 
 }
